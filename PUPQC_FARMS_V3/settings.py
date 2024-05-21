783d89caa428b7abf498ff255abd8bd2e8683982
FrancisPog1 <allanbelenisko@gmail.com>
2024-05-21 18:46:49 +0800

    I added the apps

--- PUPQC_FARMS_V3/settings.py
+++ PUPQC_FARMS_V3/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'dashboard',
+    'requirements',
+    'submissions',
+    'monitoring',
+    'reports',
+    
 ]
 
 MIDDLEWARE = [
--- PUPQC_FARMS_V3/urls.py
+++ PUPQC_FARMS_V3/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('accounts/', include('accounts.urls', namespace='accounts')), 
+    path('dashboard/', include('dashboard.urls', namespace='dashboard')),
+    path('requirements/', include('requirements.urls', namespace= 'requirements')),
+    path('submissions/', include('submissions.urls', namespace ='submissions')),
+    path('monitoring/', include('monitoring.urls', namespace ='monitoring')),
+    path('reports/', include('reports.urls', namespace ='reports')),
+
 ]
